3a58cb39716b8ee684ea84b46c0a19c296f1f561
Luis Yoshida <luis.yoshida@gmail.com>
2012-11-11 00:54:28 -0200

    Chapter 10 complete

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,5 +2,9 @@ class LineItem < ActiveRecord::Base
   attr_accessible :product_id, :cart_id
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end
 
